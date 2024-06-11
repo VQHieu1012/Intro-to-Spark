68b50798f81e1358018d42ee0d67a600fac98c15
VQHieu1012 <vuquanghieuaz@gmail.com>
2024-06-12 02:34:50 +0700

    RDDS

---
+++ RDDs/ratings-counter.py
@@ -0,0 +1,18 @@
+import findspark
+findspark.init(r'C:\spark')
+
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile("data\\ml-100k\\u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
+
+# cmd: spark-submit ratings-counter.py
